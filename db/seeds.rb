3b5118c7dde404ac999dd872037022258eb96ba9
Sam Wells <samwells95@gmail.com>
2023-01-23 19:52:42 -0800

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "SammyBoy", password: "SuperSecure")
+User.create(username: "Chef", password: "SuperSecure1")
+User.create(username: "Hello", password: "SuperSecure2")
+User.create(username: "Epic", password: "SuperSecure3")
+User.create(username: "There", password: "SuperSecure4")
