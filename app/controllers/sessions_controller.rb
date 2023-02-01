73a14a8ca4ebab172a4e952852f33e6d9759ad84
Sam Wells <samwells95@gmail.com>
2023-02-01 06:40:25 -0800

    Prevent nav to login when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
+
   def new; end
 
   def create
@@ -19,5 +21,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'Successfully logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    return unless logged_in?
+
+    flash[:error] = 'You are already logged in'
+    redirect_to root_path
   end
 end
