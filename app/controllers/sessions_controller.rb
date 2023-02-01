1f4477d72edeae3b8ae402b5cc94147a48d80da6
Sam Wells <samwells95@gmail.com>
2023-01-31 18:17:08 -0800

    Add logic to handle login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,22 @@
 
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Incorrect login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successully logged out'
+    redirect_to login_path
+  end
 end
